1e1bbdd2998e65c48a5c3d8aa6c47b18ecb58459
tnat410 <tnat410@gmail.com>
2018-12-04 06:35:32 -0500

    Create get_restaurants.py

---
+++ get_restaurants.py
@@ -0,0 +1,21 @@
+import numpy as np
+import json
+
+def split(a):
+	result = [x.strip() for x in a.split(',')]
+	return result
+
+json_file_in ='yelp_academic_dataset_business.json'
+json_file_out ='yelp_academic_dataset_restaurants.json'
+	
+with open(json_file_in, "r", encoding="utf8") as input:
+	with open(json_file_out, "w", encoding="utf8") as output:
+		for line in input:	
+			data = json.loads(line)	
+			if(data['categories'] != None):
+				for i in split(data['categories']):
+					if(i == 'Restaurants' or i == 'Food'):
+						output.write(line)	
+						break
+			
+
